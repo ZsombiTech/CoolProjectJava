1072fc513b093175f5e620b36a1e80c48a1b7080
ZSOMBI_PC\Zsombi <tatarzsombi@yahoo.com>
2021-04-26 16:58:21 +0200

    nyelv valtoztatasa feluletrol. nem szep de mukodik :)

--- src/main/java/hu/my/coolproject/controllers/BaseController.java
+++ src/main/java/hu/my/coolproject/controllers/BaseController.java
@@ -1,11 +1,18 @@
 package hu.my.coolproject.controllers;
 
+import java.io.IOException;
+import java.util.Locale;
+
+import org.zkoss.util.Locales;
+import org.zkoss.util.resource.Labels;
+import org.zkoss.web.Attributes;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.Session;
 import org.zkoss.zk.ui.Sessions;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
+import org.zkoss.zk.ui.util.Clients;
 import org.zkoss.zkplus.spring.DelegatingVariableResolver;
 
 import hu.my.coolproject.domain.User;
@@ -17,11 +24,16 @@ public class BaseController <T extends Component> extends SelectorComposer<T> {
 	
 	private static final String MY_STORED_DATA = "my_stored_data";
 	private static final String LOGIN_WIN = "loginWin";
+    public static final String ENG = "en_US";
+    public static final String HUN = "hu_HU";
 	
 	@Override
 	public void doAfterCompose(T comp) throws Exception{
 		super.doAfterCompose(comp);
-		
+        if(Sessions.getCurrent().getAttribute(Attributes.PREFERRED_LOCALE) == null){
+            //changeLocale(Locales.getCurrent().toString());  igy lehet lekerdezni, hogy a felhasznalonal milyen nyelvi beallitasok vannak es az szerint allitani. itt csekkolni kell hogy a projektunk tamogatja-e a nyelvet. ha nem akkor kell egy alapertelmezett.
+        	changeLocale(HUN); //alapertelmezett magyar
+        }
 		if (!LOGIN_WIN.equals(comp.getId()) && getSessionUser() == null) {
 			Executions.sendRedirect("/login.zul");
 		}
@@ -41,6 +53,17 @@ public class BaseController <T extends Component> extends SelectorComposer<T> {
 		Session session = Sessions.getCurrent();
 		Object o = session.getAttribute(MY_STORED_DATA);
 		return o == null ? null : (User)o;
-		
 	}
+	
+    public void changeLocale(String localeString) throws IOException {
+    	Session session = Sessions.getCurrent();
+    	if(session.getAttribute(Attributes.PREFERRED_LOCALE) != null && session.getAttribute(Attributes.PREFERRED_LOCALE).equals(localeString)) {
+    		return;
+    	}
+    	Locale locale = localeString.length() > 2 ? new Locale(localeString.substring(0,2),localeString.substring(3)) : new Locale(localeString);  
+    	session.setAttribute(Attributes.PREFERRED_LOCALE, locale);
+    	//Clients.reloadMessages(locale); ez az oldal frissitese nelkul csereli le a nyelvet. persze ha nem frissul nem latsz sokat de az uj betoltott szovegek mar mas nyelvuek
+    	//Locales.setThreadLocal(locale);
+    	Executions.sendRedirect(null);
+    }
 }
--- src/main/java/hu/my/coolproject/controllers/LoginController.java
+++ src/main/java/hu/my/coolproject/controllers/LoginController.java
@@ -1,8 +1,9 @@
 package hu.my.coolproject.controllers;
 
-import java.util.Locale;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.zkoss.util.Locales;
 import org.zkoss.util.resource.Labels;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.event.Event;
@@ -12,7 +13,10 @@ import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
 import org.zkoss.zk.ui.util.Notification;
 import org.zkoss.zul.Button;
-import org.zkoss.zul.Radiogroup;
+import org.zkoss.zul.Combobox;
+import org.zkoss.zul.Comboitem;
+import org.zkoss.zul.ListModel;
+import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Textbox;
 import org.zkoss.zul.Window;
 
@@ -34,7 +38,7 @@ public class LoginController extends BaseController<Window> {
 	private Button login;
 	
 	@Wire
-	private Radiogroup rgType;
+	private Combobox languageCmbx;
 
 	@WireVariable("userServiceImpl")
 	private transient UserService userService;
@@ -42,6 +46,17 @@ public class LoginController extends BaseController<Window> {
 	@Override
 	public void doAfterCompose(Window window) throws Exception {
 		super.doAfterCompose(window);
+		ListModelList<Comboitem> languageList = new ListModelList<>();
+		//List<Comboitem> languageList = new ArrayList<>();
+		Comboitem ci = new Comboitem();
+		ci.setLabel("HU");
+		ci.setValue(HUN);
+		languageList.add(ci);
+		ci = new Comboitem();
+		ci.setLabel("EN");
+		ci.setValue(ENG);
+		languageList.add(ci);
+		languageCmbx.setModel(languageList);
 	}
 
 	@Listen(Events.ON_CLICK + "=#login")
@@ -76,5 +91,11 @@ public class LoginController extends BaseController<Window> {
 
 	public void wrongPass() {
 		Notification.show(Labels.getLabel("Username_or_password_is_incorrect_please_try_again"), true);
+	}
+	
+	@Listen(Events.ON_SELECT + "=#languageCmbx")
+	public void selectLanguage() throws IOException {
+		changeLocale(languageCmbx.getSelectedItem().getValue());
+		languageCmbx.setSelectedItem(languageCmbx.getSelectedItem());
 	}
 }
